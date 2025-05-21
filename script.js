9ba46c7a04f50e8478f21b402db3caa4f68b1f4c
raphaelwiener <kontakt@raphael-wiener.de>
2025-05-21 14:02:52 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,12 @@ $(document).ready(function() {
   createGrid($('#playerBoard'));
   createGrid($('#computerBoard'));
 });
+
+function createGrid($container) {
+  $container.empty();
+  var letters = 'ABCDEFGHIJ'.split('');
+  var size = 10;
+  var $table = $('<table></table>');
+  var $thead = $('<thead></thead>');
+  var $headRow = $('<tr></tr>').append('<th></th>');
+}
